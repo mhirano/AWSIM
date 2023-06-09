ef126aca7b3e279b87c7ca391f2f0a4b4614cf9e
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2023-06-09 09:08:40 +0200

    wip
    Signed-off-by: Piotr Jaroszek <piotr.jaroszek@robotec.ai>

---
+++ Assets/RuntimeTests/VehicleTest.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class VehicleTest
+{
+
+    AsyncOperation sceneLoader;
+
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        yield return EditorSceneManager.LoadSceneAsync("NPCVehicleSample", LoadSceneMode.Additive);
+    }
+
+    [UnityTest]
+    public IEnumerator Movement()
+    {
+        yield return new WaitForSeconds(10);
+
+        LogAssert.Expect(LogType.Log, "Straight");
+        
+        yield return null;
+    }
+}
